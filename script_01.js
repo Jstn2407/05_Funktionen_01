20f82574c2968a2ded4d0c91d576183c0012d447
Justin <justinpessel@gmail.com>
2021-02-10 10:57:02 +0100

    Funktionen

--- script_01.js
+++ script_01.js
@@ -1,2 +1,42 @@
 
 /***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsrumpf (body)
+function test() 
+{
+    console.log("Hallo Justin!")    
+}
+
+//Funktionsaufruf (call)
+//test();
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+function ausgabeNamen() 
+{
+    let firstName = "Ahmad"; //Variable
+    console.log("Hallo " +  firstName + "!"); 
+}
+
+//ausgabeNamen();
+//console.log(firstName); // Fehler --> lokal
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von Außen
+
+function ausgabeNamen(firstName) 
+{
+    if (firstName == undefined) {
+        firstName = "kein Name vorhanden";
+        
+    }
+    console.log("Hallo " +  firstName + "!"); 
+}
+
+ausgabeNamen("Ahmad"); //Argumente / externe Daten
+ausgabeNamen("Jenny");
+ausgabeNamen(); //Fehler, keine Daten .....
+ausgabeNamen(prompt("Bitte Namen eingeben!"));
+
